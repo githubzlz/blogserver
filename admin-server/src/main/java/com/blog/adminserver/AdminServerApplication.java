1905d9affe28e9f7d5747bfcb0e85045652628c3
朱林忠 <sdbz_zlz@163.com>
2019-10-31 08:15:45 +0800

    创建权限管理员中心，删除test测试类

---
+++ admin-server/src/main/java/com/blog/adminserver/AdminServerApplication.java
@@ -0,0 +1,17 @@
+package com.blog.adminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
